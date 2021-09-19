2bea5aa25bc4cbb96430748d24b9ff9fd75cb68b
wissemayadi <wissem-ayadi@hotmail.com>
2021-09-19 21:58:56 +0100

    service implementation

---
+++ ng-video-game-db/src/app/services/http.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment as env } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { APIResponse, Game } from '../models';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  constructor(private http:HttpClient) { }
+  getGameList(ordering:string,
+    search?:string):Observable<APIResponse<Game>>{
+    let params = new HttpParams().set('ordering',ordering);
+if(search){
+  params = new HttpParams().set('ordering',ordering).set('search',search);
+
+}
+return this.http.get<APIResponse<Game>>(`${env.BASE_URL}/games`, {
+  params: params,
+});  
+}
+}
+
+
+// wissou
+
+// root
--- ng-video-game-db/src/environments/environment.prod.ts
+++ ng-video-game-db/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: false,
+  BASE_URL: 'https://rawg-video-games-database.p.rapidapi.com'
 };
